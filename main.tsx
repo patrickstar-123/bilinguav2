a81ad268fd7c2d79b3a5452ce70ffc5a94e4abcb
patrickstar-123 <patrick.st4r306@gmail.com>
2026-01-06 03:14:32 +0700

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './src/App'
+import './src/styles/globals.css'
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {
+      // Service worker registration failed, but app will still work
+    })
+  })
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
